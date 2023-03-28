e554476e3c6f2987b9cf4d563385bd8cd56fdc83
Oliver <almaguero95@gmail.com>
2023-03-28 14:56:00 -0700

    changed openai api for langchain, reason is to get response message in json format

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const { Configuration, OpenAIApi } = require('openai')
-const axios = require('axios');
-const bodyParser = require('body-parser')
-require('dotenv').config();
-const configurationa = new Configuration({
-    organization: "org-fnANBh9V0ypwCz9MZxvgAwT4",
-    apiKey: process.env.API_KEY,
-});
+// const axios = require('axios');
+import axios from 'axios';
+import { OpenAI, PromptTemplate } from "langchain";
+import { StructuredOutputParser, OutputFixingParser } from "langchain/output_parsers";
+import { SerpAPI, Calculator } from "langchain/tools";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const express = require('express')
+// const configurationa = new Configuration({
+//     organization: "org-fnANBh9V0ypwCz9MZxvgAwT4",
+//     apiKey: process.env.API_KEY,
+// });
+
+import express from 'express';
+import cors from 'cors';
+// const express = require('express')
 const app = express()
 
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const corsOptions = {
   origin: '*',
@@ -19,7 +25,9 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-const querystring = require('querystring');
+// const querystring = require('querystring');
+import querystring from 'querystring';
+
 
 
 
@@ -33,7 +41,7 @@ app.options("*",function(req,res,next){
     res.status(200).end();
 });
   
-  var distDir = __dirname + "/dist/angular-test";
+  // var distDir = __dirname + "/dist/angular-test";
   app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -48,42 +56,76 @@ app.options("*",function(req,res,next){
 // app.use(express.static(distDir));
 
 const PORT = process.env.PORT || 3000;
-const openai = new OpenAIApi(configurationa);
+// const model = new OpenAI({temperature: 0.5});
+// const res = await model.call("What is the capital city of france");
 const giphyUrl = 'https://api.giphy.com/v1/gifs/search';
 
+//LANGCHAIN STARTS ////////////////////////////////////////////////////////////////////
+
+const model = new OpenAI({openAIApiKey: process.env.OPENAI_API_KEY, temperature: 0.5});
+// const tools = [new SerpAPI()];
+
+app.post('/getSongs', async(req, res) => {
+  const parser = StructuredOutputParser.fromNamesAndDescriptions({
+      song: "name of the song",
+      artist: "the artist of the song",
+    });
+  
+    const formatInstructions = parser.getFormatInstructions();
+  
+    const prompt = new PromptTemplate({
+      template:
+        `Give the user recommendations as best as possible.\n{format_instructions}\n{question}`,
+      inputVariables: ["question"],
+      partialVariables: { format_instructions: formatInstructions },
+    });
+  
+    const input = await prompt.format({
+      question: `Recommend me a list of 7 songs if my favorite song is ${req.body.song} by ${req.body.artist}, try to avoid songs from the same artist, return an array of objects`,
+    });
+    const response = await model.call(input);
+    var newStr = response.replace(/`/g, "").replace(/json/g, "")
+    res.send(JSON.stringify(newStr));
+});
+
+
+//LANGCHAIN FINISH ////////////////////////////////////////////////////////////////////
 
 
 app.get('/test', (req, res) => {
     res.send('Hello World');
 })
-// const response = await openai.listEngines();
-app.post('/songs', async(req, res) => {
-    let data = {
-        "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content":
-        `Recommend me a list of 10 songs if my favorite song is ${req.body.song} from ${req.body.artist}, do not include any explanations, also try to not add songs from the same artist. 
-        Only Return a JSON object with the songs in the following format:
-        {"data": [
-            {
-                    "songName": "the name of the song",
-                "songArtist": "the name of the artist"
-            }
-        ]
-        };
 
-        Do not explain anything else.
-        `}],
-    }
-    axios.post('https://api.openai.com/v1/chat/completions', data, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.API_KEY}`,
-        }
-    })
-    .then(elem => {
-        res.send(elem.data.choices[0]);
-    }).catch(err => res.send(err));
-})
+/////////////// REPLACED FOR LANGCHAIN ///////////////// BUT I STILL WANT THE CODE FOR FUTURE REFERENCE
+
+// const response = await openai.listEngines();
+// app.post('/songs', async(req, res) => {
+//     let data = {
+//         "model": "gpt-3.5-turbo",
+//         "messages": [{"role": "user", "content":
+//         `Recommend me a list of 10 songs if my favorite song is ${req.body.song} from ${req.body.artist}, do not include any explanations, also try to not add songs from the same artist. 
+//         Only Return a JSON object with the songs in the following format:
+//         {"data": [
+//             {
+//                     "songName": "the name of the song",
+//                 "songArtist": "the name of the artist"
+//             }
+//         ]
+//         };
+
+//         Do not explain anything else.
+//         `}],
+//     }
+//     axios.post('https://api.openai.com/v1/chat/completions', data, {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+//         }
+//     })
+//     .then(elem => {
+//         res.send(elem.data.choices[0]);
+//     }).catch(err => res.send(err));
+// })
 
 app.get('/auth_window', (req, res) => {
   var client_id = process.env.SPOTIFY_CLIENT_API_KEY;
@@ -127,7 +169,6 @@ app.get('/spotify_token', (req, res) => {
 
 
     app.get("/gif", (req, res) => {
-      console.log("llega a gif");
       let gif;
       axios.get(`${giphyUrl}?api_key=${process.env.GIPHY_API_KEY}&q=robots&limit=15`)
       .then(elem => {
